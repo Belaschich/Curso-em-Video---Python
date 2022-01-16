eb719aeb0352bae1a46acd8daa205e77f0c6cb43
Izabela Schich <izabela.schick@gmail.com>
2022-01-16 20:27:04 -0300

    Desafios feitos logos nos primeiros videos do curso.
    Desafios feitos logos nos primeiros vídeos do curso.

---
+++ Desafio/Desafio2.py
@@ -0,0 +1,7 @@
+#Crie um script python que leia o dia, o mês e o ano de nacimento de uma pessoa
+#e mostre uma mensagem com a data formatada.
+
+dia=input('Digite o Dia do seu nascimento! ')
+mes=input('Digite o Mês do seu nascimento! ')
+ano=input('Digite o Ano do seu nascimento! ')
+print('Você nasceu no dia', dia, 'de', mes, 'de', ano,'. Correto?')
---
+++ Desafio/Desafio3.py
@@ -0,0 +1,8 @@
+#Crie um script Python que leia dois números e tente mostrar a soma entre eles.
+
+##Desafio foi feito para sair exatamente como está!!
+
+numero1= input('Digite um número: ')
+numero2= input('Digite o segundo número: ')
+print(numero1, '+', numero2, '=', numero1+numero2)
+
