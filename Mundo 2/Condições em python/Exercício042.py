cfb6e575d2a5103c2ab464aac57fadcfa9c14f18
Izabela Schich <izabela.schick@gmail.com>
2022-04-05 19:46:40 -0300

    .

--- Mundo 2/Condições em python/Exercício042.py
+++ Mundo 2/Condições em python/Exercício042.py
@@ -12,7 +12,11 @@ r2 = float(input("Digite um Valor: "))
 r3 = float(input("Digite um Valor: "))
 
 if r1 + r2 > r3 and r2 + r3 > r1 and r3 + r1 > r2:
-    if
-    print("Os valores {}, {} e {} podem formar um triângulo".format(r1, r2, r3))
+    if r1 == r2 == r3:
+        print("Os valores {}, {} e {} podem formar um triângulo EQUILÁTERO".format(r1, r2, r3))
+    elif r1 == r2 != r3 or r2 == r3 != r1 or r3 == r1 != r2:
+        print("Os valores {}, {} e {} podem formar um triângulo ISÓSCELES".format(r1, r2, r3))
+    else:
+        print("Os valores {}, {} e {} podem formar um triângulo ESCALENO".format(r1, r2, r3))
 else:
     print("Os valores {}, {} e {} não podem formar um triângulo".format(r1, r2, r3))
