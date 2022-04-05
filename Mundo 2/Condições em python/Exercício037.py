22fff1b2d0dc8288033bf3e22b1066342263cfea
Izabela Schich <izabela.schick@gmail.com>
2022-04-05 17:26:46 -0300

    .

---
+++ Mundo 2/Condições em python/Exercício037.py
@@ -0,0 +1,12 @@
+'''
+Escreva um programa em Python que leia um número inteiro qualquer e peça para 
+o usuário escolher qual será a base de conversão: 
+1 para binário, 2 para octal e 3 para hexadecimal.
+'''
+
+n= int(input("Digite um número qualquer: "))
+menu: int(input("Qual base de conversão, gostaria de usar?", end=  
+            "1 para binário", end =
+            "2 para octal", end=
+            "3 para hexadecimal"))
+
