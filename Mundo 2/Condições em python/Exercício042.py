1349d7dd18621daa6ba3f1434cdc87064459cb24
Izabela Schich <izabela.schick@gmail.com>
2022-04-05 18:43:26 -0300

    .

--- Mundo 2/Condições em python/Exercício042.py
+++ Mundo 2/Condições em python/Exercício042.py
@@ -7,3 +7,12 @@ Refaça o DESAFIO 35 dos triângulos, acrescentando o recurso de mostrar que tip
 
 – ESCALENO: todos os lados diferentes
 '''
+r1 = float(input("Digite um valor: "))
+r2 = float(input("Digite um Valor: "))
+r3 = float(input("Digite um Valor: "))
+
+if r1 + r2 > r3 and r2 + r3 > r1 and r3 + r1 > r2:
+    if
+    print("Os valores {}, {} e {} podem formar um triângulo".format(r1, r2, r3))
+else:
+    print("Os valores {}, {} e {} não podem formar um triângulo".format(r1, r2, r3))
