56b0013cfdfad453e248682d2deef18008cd83b7
Izabela Schich <izabela.schick@gmail.com>
2022-04-06 00:09:28 -0300

    .

--- Mundo 2/Condições em python/Exercício045.py
+++ Mundo 2/Condições em python/Exercício045.py
@@ -1,3 +1,26 @@
 '''
 Crie um programa que faça o computador jogar Jokenpô com você.
 '''
+
+import random
+Pedra = 1
+Papel = 2
+Tesoura = 3
+
+jogador= int(input("Escolha entre: \n" 
+    "1- Pedra \n" 
+    "2- Papel \n" 
+    "3- Tesoura \n" ))
+
+pc = [1, 2, 3]
+
+jogo = random.choice(pc)
+
+if jogador == jogo:
+    print("O computador jogou {} e você  {}, nulo".format(jogo, jogador))
+elif jogador == 1 and jogo == 3:
+    print("O computador jogou {} e você  {}, Você Ganhou!!!".format(jogo, jogador))
+elif jogador == 2 and jogo == 3:
+    print("O computador jogou {} e você  {}, O Computador Ganhou!!!".format(jogo, jogador))
+else:
+    print("O computador jogou {} e você  {}, Você Ganhou!!!".format(jogo, jogador))
