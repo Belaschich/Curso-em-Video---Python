4a430e0d46e65d0c1103dff6c20049a6591dea8f
Izabela Schich <izabela.schick@gmail.com>
2022-04-19 18:06:11 -0300

    .

---
+++ Mundo 2/Repetições em Python/Exercício056.py
@@ -0,0 +1,37 @@
+'''
+Desenvolva um programa que leia o nome, idade e sexo de 4 pessoas. 
+No final do programa, mostre: a média de idade do grupo, 
+qual é o nome do homem mais velho e quantas mulheres têm menos de 20 anos.
+'''
+from datetime import date
+atual = date.today().year
+contid = 0
+idmaior = 0
+nmaior = []
+contsx = 0
+for x in range (1, 5):
+    nome = input('Digite seu nome: ')
+    idade = int(input('Digite a sua idade: '))
+    sx = input('Digite seu genero F- Feminino, M- masculino: ').strip().upper()
+
+    if idade > 0:
+        contid +=idade
+        md = contid/x
+    
+    if idade < 20 and sx == 'F':
+        contsx +=1
+    
+    if idade > idmaior and sx =='M':
+        idmaior = idade
+        nmaior = nome
+
+print(f'A média de idade do grupo é {md}')
+print(f'O homem mais velho tem {idmaior} anos e se chama: {nmaior}')
+print(f'No grupo existem {contsx} mulheres com menos de 20 anos') 
+
+        
+
+    
+        
+
+
